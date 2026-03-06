aa774201eb6139aba3a3898d71cd90f216ce134d
LahariBS <laharibsuvarna@gmail.com>
2026-03-06 20:55:11 +0530

    Refactor service worker for PWA caching

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,26 @@
-const CACHE_NAME = 'my-cache-v1';
+const CACHE_NAME = "my-pwa-cache-v1";
+
 const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'app.js',
-  'icon-192.png',
-  'icon-512.png',
-  'icon.png',
-  // add your CSS or other files here
+"./",
+"index.html",
+"manifest.json",
+"app.js",
+"icon-192.png",
+"icon-512.png"
 ];
 
-// Install event: cache files
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install",event=>{
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll(urlsToCache);
+})
+);
 });
 
-// Activate event: clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch event: serve cached files if available
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch",event=>{
+event.respondWith(
+caches.match(event.request).then(response=>{
+return response || fetch(event.request);
+})
+);
 });
